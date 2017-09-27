dd19db526a217a9632f7f6e64c3b6cb39c74ee2d
PRINCEOTI <princeotti5@gmail.com>
2017-09-27 09:44:05 -0400

    finish excercis_1

---
+++ excercise1.rb
@@ -0,0 +1,13 @@
+# Define a method called double that accepts an argument called my_number and returns that number multiplied by 2.
+
+# Try calling it multiple times and pass in different numbers each time.
+# in this situation you will have to define it as double and renaming it
+#my number using open and close blackets "()", now my_number is just as double
+# so when you choose a number for double it is same as my_number and you can multiply
+#that by 2 as stated by the question and terminal will print the answer.
+
+def double (my_number)
+puts my_number * 2
+end
+
+double(4)
